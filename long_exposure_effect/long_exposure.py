db58c9f07dacc2c3aeba1e8428cfcf3964fc6e41
Daniel Tang <danieltang@apple.com>
2017-12-16 16:23:27 -0800

    added the first computer vision project for fun

---
+++ long_exposure_effect/long_exposure.py
@@ -0,0 +1,58 @@
+# import the necessary packages
+import argparse
+import cv2
+
+
+
+def apply_long_exposure_effect(input_video, output_img):
+
+	# initialize the Red, Green, and Blue channel averages, along with# the total number of frames read from the file
+	channels = None
+	(r_channel, g_channel, b_channel) = (None, None, None)
+	frames = 0
+
+	print("Opening video file ...")
+	stream = cv2.VideoCapture(input_video)
+	print("Computing frame averages (this will take awhile)...")
+
+	# loop over frames from the video file stream
+	while True:
+		# grab the frame from the file stream
+		(grabbed, frame) = stream.read()
+		# if the frame was not grabbed, then we have reached the end of the file	
+		if not grabbed:
+			break
+		# otherwise, split the frmae into its respective channels
+		current_channels = cv2.split(frame.astype("float"))
+		n_channels = len(current_channels)
+		# if the frame averages are None, initialize them	
+		if channels is None:
+			channels = current_channels
+		# otherwise, compute the weighted average between the history of frames and the current frames	
+		else:
+			for k in range(n_channels):
+				channels[k] = ((frames * channels[k]) + (1 * current_channels[k])) / (frames + 1.0)
+		# increment the frame number by one
+		frames += 1
+
+	# merge the RGB averages together and write the output image to disk
+	avg = cv2.merge(channels).astype("uint8")
+	cv2.imwrite(output_img, avg)
+	# do a bit of cleanup on the file pointer
+	stream.release()
+
+def parse_arguments():
+	# construct the argument parse and parse the argumentsap = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-v", "--video", required=True, help="path to input video file")
+	parser.add_argument("-o", "--output", required=True, help="path to output png file with long exposure effect")
+	args = parser.parse_args()
+
+	return args
+
+def main():
+	args = parse_arguments()
+	apply_long_exposure_effect(args.video, args.output)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,10 @@
+#**Fun Projects on Computer Vison** 
+
+## Long Exposure Effect
+
+One of my favorite photography techniques is long exposure, the process of creating a photo that shows the effect of passing time, something that traditional photography does not capture. This is an experiment on converting a video file into a jpg file with long time exposure effect, with OpenCV and Python.
+
+The source video file can be found at [videohive.net][https://videohive.net/item/milky-way-from-ruined-observatory/12467677]
+The output is shown below
+![Rotating starry sky](./long_exposure_effect/results/milkyway.jpg)
+
